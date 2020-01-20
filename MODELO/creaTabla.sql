3855c04b9fb17201bd84490bbbeddec9b06182bb
Carlos F. Batalla <carlosfranz@ucla.edu.ve>
2020-01-20 11:00:35 -0400

    se suben nvos. archivos

--- MODELO/creaTabla.sql
+++ MODELO/creaTabla.sql
@@ -5,7 +5,7 @@ CREATE TABLE tb_user (
           ,username VARCHAR(30)
           ,firstname VARCHAR(100) not null
           ,lastname VARCHAR(100) not null
-          ,email VARCHAR(100)
+          ,email VARCHAR(120)
           ,CHECK (deleted <1)
           ,CHECK (suspended <1)  
         );
